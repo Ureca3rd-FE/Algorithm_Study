43ae6d12bf777a9103c66571e049ab74a460c132
김주희 <kimjuhee@gimjuhuis-MacBook-Air.local>
2025-09-16 16:15:56 +0900

    BOJ_2562_김주희

--- week01/joooii/BOJ_2562/main.js
+++ week01/joooii/BOJ_2562/main.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const input = fs.readFileSync(0, "utf-8").trim().split("\n").map(Number);
 
-let max = Math.max(...input.map(Number));
+let max = Math.max(...input);
 let idx = input.indexOf(max);
 
 console.log(max);
