cb361fe21e7befda9d6f1128aff37230ac4d3544
40food <40food@naver.com>
2025-09-16 14:52:02 +0900

    BOJ_2562_박승연

---
+++ week01/40food/BOJ_2562/Main.java
@@ -0,0 +1,22 @@
+package algo_study;
+
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		int index=-1;
+		int max=0;
+		
+		for(int i=0;i<9;i++) {
+			int num=sc.nextInt();
+			if(num>max) {
+				max=num;
+				index=i+1;
+			}
+		}
+		
+		System.out.println(max);
+		System.out.println(index);
+    }
+}
