6d75fece5ba1850e0f9231a56acdcd6fd07b82d7
Zoo2-bi <bagjui068@gmail.com>
2025-09-16 11:21:36 +0900

    2557

---
+++ week01/Zoo2-bi/BOJ_2557/Main.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const i = "Hello World!";
+
+console.log(i);
