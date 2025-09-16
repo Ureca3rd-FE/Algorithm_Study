a8efce642e470e6841e9ea56822ecb6087ec57c5
40food <40food@naver.com>
2025-09-16 16:31:34 +0900

    BOJ_11720_박승연_수정

--- week01/40food/BOJ_11720/Main.java
+++ week01/40food/BOJ_11720/Main.java
@@ -6,9 +6,10 @@ public class Main{
 	public static void main(String args[]){
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
+		String[] s=sc.next().split("");
 		int total=0;
 		for(int i=0;i<n;i++) {
-			total+=sc.nextInt();
+			total+=Integer.parseInt(s[i]);
 		}
 		System.out.println(total);
 	}
